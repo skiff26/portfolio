26bff82159a760fddcd58ca990d74b3032f3d56f
Artem <artem.kulchitsky@gmail.com>
2023-03-25 15:53:17 +0100

    fix allworks

---
+++ src/components/TheAllWorks.vue
@@ -0,0 +1,36 @@
+<template>
+	<main class="page">
+		<div class="works">
+			<h2 class="works__title">My works</h2>
+			<div class="works__content"> 
+				<ul class="works__cards">
+					<li class="works__card card" @click="$router.push(`/work/${work.id}`)" v-for="(work) in works" :key="work">
+						<h4 class="card__title">{{ work.title }}</h4>
+						<div class="card__img"><img :src="`./posts/${work.path}/cover.png`" :alt="work.alt"></div>
+						<div class="card__info">
+							<p class="card__theme">{{ work.theme }}</p>
+							<p class="card__year">{{ work.year }}</p>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</main>
+</template>
+<style scoped lang="scss">
+.page {
+	scroll-behavior: smooth;
+	background: rgb(255,255,255);
+	background: linear-gradient(180deg, rgba(255,255,255,0.8995973389355743) 0%, rgba(240,246,251,0.8323704481792717) 35%, rgba(240,246,251,1) 75%, rgba(255,255,255,1) 100%);
+}	
+</style>
+<script>
+import works from '../works'
+export default {
+	data(){
+		return {
+			works: [...works.posts, ...works.otherPosts]
+		}
+	}
+}
+</script>
--- src/views/WorksView.vue
+++ src/views/WorksView.vue
@@ -2,49 +2,24 @@
 	<div class="wrapper">
 		<TheHeader v-if="!isSmall" :transparent="transparent"/>
 		<TheHeaderMobile v-else :transparent="transparent"/>
-		<main class="page">
-			<div class="works">
-				<h2 class="works__title">My works</h2>
-				<div class="works__content"> 
-					<ul class="works__cards">
-						<li class="works__card card" @click="$router.push(`/work/${work.id}`)" v-for="(work) in works" :key="work">
-							<h4 class="card__title">{{ work.title }}</h4>
-							<div class="card__img"><img :src="`/posts/${work.path}/cover.png`" alt=""></div>
-							<div class="card__info">
-								<p class="card__theme">{{ work.theme }}</p>
-								<p class="card__year">{{ work.year }}</p>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</main>
+		<TheAllWorks />
 		<TheFooter :transparent="transparent"/>
 	</div>
 </template>
 
-<style scoped lang="scss">
-.page {
-	scroll-behavior: smooth;
-	background: rgb(255,255,255);
-	background: linear-gradient(180deg, rgba(255,255,255,0.8995973389355743) 0%, rgba(240,246,251,0.8323704481792717) 35%, rgba(240,246,251,1) 75%, rgba(255,255,255,1) 100%);
-}	
-</style>
-
 <script>
-import works from '../works'
 import TheHeader from '../components/TheHeader.vue'
 import TheHeaderMobile from '../components/TheHeaderMobile.vue'
 import TheFooter from '../components/TheFooter.vue'
+import TheAllWorks from '../components/TheAllWorks.vue'
 export default {
 	components: {
-		TheHeader, TheFooter, TheHeaderMobile
+		TheHeader, TheFooter, TheHeaderMobile, TheAllWorks
 	},
 	data(){
 		return {
 			isSmall: false,
 			transparent: false,
-			works: [...works.posts, ...works.otherPosts]
 		}
 	},
 	methods: {
