c4263f3182b309df7a1020c93f0f738116479579
Artem <artem.kulchitsky@gmail.com>
2023-09-06 08:26:44 +0200

    Update base path, seo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio-new/',
+  base: '/portfolio/',
   plugins: [
     vue(),
   ],
