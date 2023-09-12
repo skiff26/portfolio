7e71839e5fd72d5f7264a850f8eb15983660f83a
Artem <artem.kulchitsky@gmail.com>
2023-09-12 07:53:33 +0200

    Added locale in router, middleware

---
+++ src/i18n/translation.ts
@@ -0,0 +1,23 @@
+import i18n from '@/i18n'
+
+const supportedLocales = (locale: string) => ['en', 'uk'].includes(locale)
+
+export const getBaseLocale = () => localStorage.getItem('user-locale') || i18n.global.locale.value
+
+export const changeLocale = (code: string) => {
+	i18n.global.locale.value = code 
+	document.documentElement.setAttribute('lang', code)
+	localStorage.setItem('user-locale', code)
+}
+
+export const routeMiddleware = (to: any, _from: any, next: any) => {
+	const paramLocale: string = to.params.locale
+
+	if (!supportedLocales(paramLocale)) {
+		return next(getBaseLocale())
+	}
+
+	changeLocale(paramLocale)
+
+	return next()
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { RouterView, createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import { routeMiddleware } from '@/i18n/translation'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+		path: '/:locale?',
+		component: RouterView,
+		beforeEnter: routeMiddleware,
+		children: [
+			{
+				path: '',
+				name: 'home',
+				component: HomeView
+			}
+		]
     }
   ]
 })
